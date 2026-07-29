8828ebb90a0dbd9d993a29f245b4430dbb36bc0d
EijiGorilla <emasuz@hotmail.com>
2026-07-29 08:51:33 +0800

    Updates

---
+++ assets/OGCFeatureLayerView-BJsbf-Yr.js
@@ -0,0 +1,1 @@
+import{$ as l,a1 as a}from"./index-CLzMNG-b.js";import{B as r}from"./FeatureLikeLayerView-C13w8JPI.js";let t=class extends r{constructor(){super(...arguments),this.layer=null}get availableFields(){return this.layer.fieldsIndex.fields.map(e=>e.name)}};l([a({readOnly:!0})],t.prototype,"availableFields",null);export{t as l};
